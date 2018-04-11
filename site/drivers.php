773d04790f9fb92649162609d34bffb91a0dc669
maxpilotto <maxpilotto99@gmail.com>
2018-04-11 22:06:49 +0200

    update drivers' layout

--- site/drivers.php
+++ site/drivers.php
@@ -29,7 +29,7 @@ include_once("connection.php");
 			}
 
             if (mysqli_num_rows($result) == 0){
-    			echo "Driver not found";
+                echo "<h1><b>Driver not found</b></h1>";
     		}else{
     			$row = mysqli_fetch_assoc($result);
 
@@ -37,14 +37,42 @@ include_once("connection.php");
 					header("Location: pageNotFound.php");
 					return;
 				}
-    			echo "<b>Name:</b> $row[name]<br />";
-    			echo "<b>Surname:</b> $row[surname]<br />";
+
+				echo "
+                <table class='mui-table'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Surname</th>
+                            <th>Shipments to do</th>
+                        </tr>
+                    </thead>
+                <tbody>";
+
+				$shipments = mysqli_query($conn,"SELECT * FROM shipments WHERE driver = $row[id]");
+				$toDo = 0;
+
+				foreach($shipments as $shipment){
+					if ($shipment["status"] == '2' or $shipment["status"] == '4'){
+						$toDo++;
+					}
+				}
+
+				echo "<tr>";
+				echo "<td>$row[name]</td>";
+				echo "<td>$row[surname]</td>";
+				echo "<td>$toDo</td>";
+				echo "</tr>";
+
+                echo "
+                    </tbody>
+                </table>";
     		}
         }else{
 		    $result = mysqli_query($conn,"SELECT * FROM drivers WHERE company = $_SESSION[company]");
 
             if (mysqli_num_rows($result) == 0){
-    			echo "No drivers found";
+                echo "<h1><b>No drivers found</b></h1>";
     		}else{
     			$row = mysqli_fetch_assoc($result);
 
