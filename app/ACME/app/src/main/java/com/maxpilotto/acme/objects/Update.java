8a88c72ab0502223514391d94de5fb22c57ffd82
maxpilotto <maxpilotto99@gmail.com>
2018-04-24 21:05:59 +0200

    add app draft

---
+++ app/ACME/app/src/main/java/com/maxpilotto/acme/objects/Update.java
@@ -0,0 +1,20 @@
+package com.maxpilotto.acme.objects;
+
+import java.sql.Date;
+import java.sql.Time;
+
+/**
+ * Project: ACME
+ * Created by: Max
+ * Date: 24/04/2018 @ 20:47
+ * Package: com.maxpilotto.acme.objects
+ */
+public class Update {
+    private Date date;
+    private Time time;
+    private float latitude;
+    private float longitude;
+    private int speed;
+    private int shipment;
+    private String place;
+}
