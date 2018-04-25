c183d7cbea4453f9a5befb7613fbe4d6a9e4766b
maxpilotto <maxpilotto99@gmail.com>
2018-04-25 23:08:42 +0200

    add Update class

--- app/ACME/app/src/main/java/com/maxpilotto/acme/objects/Update.java
+++ app/ACME/app/src/main/java/com/maxpilotto/acme/objects/Update.java
@@ -16,5 +16,61 @@ public class Update {
     private float longitude;
     private int speed;
     private int shipment;
-    private String place;
+
+    public Update(Date date, Time time, float latitude, float longitude, int speed, int shipment) {
+        this.date = date;
+        this.time = time;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.speed = speed;
+        this.shipment = shipment;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getShipment() {
+        return shipment;
+    }
+
+    public void setShipment(int shipment) {
+        this.shipment = shipment;
+    }
 }
