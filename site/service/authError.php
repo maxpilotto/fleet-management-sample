83b944fd03fba67f721a5fa68d42625ab1474973
maxpilotto <maxpilotto99@gmail.com>
2018-04-26 12:00:03 +0200

    add authentication to the services

---
+++ site/service/authError.php
@@ -0,0 +1,8 @@
+<?php 
+	$resp = array(
+		"status" => 400,
+		"message" => "Unauthorized access"
+	);
+	
+	echo json_encode($resp);
+?>
--- site/service/requestDriverShipment.php
+++ site/service/requestDriverShipment.php
@@ -1,1 +1,19 @@
-<?
+<?php
+	include_once("../connection.php");
+	$result = $mysqli_query($conn,"SELECT * FROM accounts WHERE username = $_POST[username] AND passwd = $_POST[passwd]");
+	
+	if (mysqli_num_rows($result) == 0){
+		include("authError.php");
+	}else{
+		$query = mysqli_query($conn,"SELECT * FROM shipments WHERE driver = $_POST[id] AND status <> 1");
+		
+		$resp = array(
+			"status" => 200,
+			"message" => "Ok",
+			"error" => $conn->error,
+			"shipmentId" => $query["id"]
+		);
+		
+		echo json_encode($resp);
+	}
+?>
