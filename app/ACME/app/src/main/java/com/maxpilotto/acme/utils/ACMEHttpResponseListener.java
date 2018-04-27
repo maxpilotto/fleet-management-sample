dc1cfe03fb5d893ba692353592bbe09ed6c2ca32
maxpilotto <maxpilotto99@gmail.com>
2018-04-27 20:59:01 +0200

    rename HttpResponseListener

---
+++ app/ACME/app/src/main/java/com/maxpilotto/acme/utils/ACMEHttpResponseListener.java
@@ -0,0 +1,11 @@
+package com.maxpilotto.acme.utils;
+
+/**
+ * Project: ACME
+ * Created by: Max
+ * Date: 27/04/2018 @ 17:36
+ * Package: com.maxpilotto.acme.utils
+ */
+public interface ACMEHttpResponseListener {
+    void onResponseReceived(String response);
+}
