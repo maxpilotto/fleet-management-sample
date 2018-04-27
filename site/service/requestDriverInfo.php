f79fbddb316d4a863dba50e668bfce7e2fbc1222
maxpilotto <maxpilotto99@gmail.com>
2018-04-27 20:55:45 +0200

    fix sql query in requestDriverInfo

--- site/service/requestDriverInfo.php
+++ site/service/requestDriverInfo.php
@@ -1,18 +1,16 @@
 <?php
 	include_once("../connection.php");
-	$result = $mysqli_query($conn,"SELECT * FROM accounts WHERE username = $_POST[username] AND passwd = $_POST[passwd]");
+	$result = mysqli_query($conn,"SELECT a.*,d.id as driverId FROM accounts as a,drivers as d WHERE d.account = a.id AND username = '$_POST[username]' AND passwd = '$_POST[passwd]'");
+
+	//TODO Hash all passwords
 
 	if (mysqli_num_rows($result) == 0){
 		echo json_encode( createHttpResponse(400,"Unauthorized access","User not found") );
 	}else{
-		$query = mysqli_query($conn,"SELECT * FROM shipments WHERE driver = $_POST[id] AND status <> 1");
-/*
-		$resp = array(
-			"status" => 200,
-			"message" => "Ok",
-			"error" => $query->error,
-			"shipmentId" => $query["id"]
-		);*/
+		$result = mysqli_fetch_assoc($result);
+		$query = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM shipments WHERE driver = $result[driverId] AND status <> 1"));
+
+		//TODO return also the driver's name and surname since the clients only sends the credentials and doesn't know which driver is logged
 
 		echo json_encode( createHttpResponse(200,"Ok","",array(
 				"shipmentId" => $query["id"]
