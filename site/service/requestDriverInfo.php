92eb56e63b167156d621063750a87b5293f29d6c
maxpilotto <maxpilotto99@gmail.com>
2018-04-27 17:42:27 +0200

    rename requestDriverShipment to requestDriverInfo

--- site/service/requestDriverInfo.php
+++ site/service/requestDriverInfo.php
@@ -1,19 +1,22 @@
 <?php
 	include_once("../connection.php");
 	$result = $mysqli_query($conn,"SELECT * FROM accounts WHERE username = $_POST[username] AND passwd = $_POST[passwd]");
-	
+
 	if (mysqli_num_rows($result) == 0){
-		include("authError.php");
+		echo json_encode( createHttpResponse(400,"Unauthorized access","User not found") );
 	}else{
 		$query = mysqli_query($conn,"SELECT * FROM shipments WHERE driver = $_POST[id] AND status <> 1");
-		
+/*
 		$resp = array(
 			"status" => 200,
 			"message" => "Ok",
-			"error" => $conn->error,
+			"error" => $query->error,
 			"shipmentId" => $query["id"]
+		);*/
+
+		echo json_encode( createHttpResponse(200,"Ok","",array(
+				"shipmentId" => $query["id"]
+			))
 		);
-		
-		echo json_encode($resp);
 	}
 ?>
