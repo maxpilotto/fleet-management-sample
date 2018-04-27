f2191ad7b9bbecc47893c851afc02489a62df5d2
maxpilotto <maxpilotto99@gmail.com>
2018-04-27 20:56:14 +0200

    add createHttpResponse in submitMovements

--- site/service/submitMovement.php
+++ site/service/submitMovement.php
@@ -1,14 +1,16 @@
-<?php 
+<?php
 	/**
 	 * This php file will handle all the data from each single truck
 	 */
-	include_once("connection.php");
+	include_once("../connection.php");
 	$user = mysqli_query($conn,"SELECT * FROM accounts WHERE username = '$_POST[user]' AND passwd = '$_POST[pass]'");
-	
+
+	//TODO Check if the user is a driver
+
 	if (mysqli_num_rows($user) == 0){
-		include("authError.php");
+		echo json_encode( createHttpResponse(400,"Unauthorized access","User not found") );
 	}else{
-		mysqli_query($conn,"INSERT INTO movements VALUES(
+		$query = mysqli_query($conn,"INSERT INTO movements VALUES(
 			null,
 			'$_POST[movDate]',
 			'$_POST[movTime]',
@@ -18,13 +20,13 @@
 			$_POST[shipment],
 			'$_POST[place]',
 		)");
-		
+/*
 		$resp = array(
 			"status" => 200,
 			"message" => "Ok",
 			"error" => $conn->error
-		);
-		
-		echo json_encode($resp);
+		);*/
+
+		echo json_encode( createHttpResponse(200,"Ok","") );
 	}
 ?>
