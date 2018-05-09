a51ca3a9317479ceadd54ce733d06d4396376688
maxpilotto <maxpilotto99@gmail.com>
2018-05-09 23:30:35 +0200

    remove createHttpResponse function

--- site/connection.php
+++ site/connection.php
@@ -1,14 +1,3 @@
 <?php
 	$conn = mysqli_connect("localhost","root","","fleet_db");
-
-	function createHttpResponse($status,$message,$error,$params = array()){
-		$response = array(
-			"status" => $status,
-			"message" => $message,
-			"error" => $error,
-			"params" => array($params)
-		);
-
-		return $response;
-	}
 ?>
